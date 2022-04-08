0b93f2b8dad174ba7b5cc8f8241c895a69550a7f
Jing Tan <jtan1993@gmail.com>
2022-04-08 22:49:52 +0100

    add dual blur

--- Assets/Scripts/CustomPostProcessing/DualKawase/DualBlurCustomVolume.cs
+++ Assets/Scripts/CustomPostProcessing/DualKawase/DualBlurCustomVolume.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
 
-public class DualBlurCustomVolume : MonoBehaviour
+// TODO: Add parameters for controlling Dual Kawase Blur
+[System.Serializable, VolumeComponentMenu("Custom PostProcessing Volumes/Dual Kawase Volume/Dual Kawase Render")]
+public class DualBlurCustomVolume : VolumeComponent, IPostProcessComponent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Range(0f, 100f), Tooltip("Blur Radius Intensity")]
+    public FloatParameter BlurRadius = new ClampedFloatParameter(3f, 0f, 10f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Range(0, 10), Tooltip("Blur Iteration Quality")]
+    public IntParameter Iteration = new ClampedIntParameter(5, 0, 10);
+
+    [Range(1, 10), Tooltip("Render Target Downscaling for Blur Depth")]
+    public FloatParameter downSample = new ClampedFloatParameter(1f, 0f, 10f);
+
+    public bool IsActive() => downSample.value > 0f;
+
+    public bool IsTileCompatible() => false;
 }
