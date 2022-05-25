1e44eaf1dce937a446c651a61fe0e365b2dd50b5
Jing Tan <jtan1993@gmail.com>
2022-05-25 14:08:41 +0100

    Update DualBlurRendererFeature.cs

--- Assets/Scripts/CustomPostProcessing/DualKawase/DualBlurRendererFeature.cs
+++ Assets/Scripts/CustomPostProcessing/DualKawase/DualBlurRendererFeature.cs
@@ -44,9 +44,9 @@ public class DualBlurRendererFeature : ScriptableRendererFeature
         };
 
         LEVEL[] my_level;
-        int maxLevel = 16;//指定一个最大值来限制申请的ID的数量，这里限制到16个，这么多肯定用不完了
+        private int maxLevel = 16;
 
-        public DualBlurRenderPass(RenderPassEvent evt)//构造函数
+        public DualBlurRenderPass(RenderPassEvent evt)
         {
             renderPassEvent = evt;
             var shader = Shader.Find("PostProcessing/DualBlur(Kawase)");
@@ -58,11 +58,11 @@ public class DualBlurRendererFeature : ScriptableRendererFeature
             DualKawaseBlurMaterial = CoreUtils.CreateEngineMaterial(shader);
         }
 
-        public void Setup(RenderTargetIdentifier currentTarget)//初始化，接收render feature传的图
+        public void Setup(RenderTargetIdentifier currentTarget)//init, receive img passed from render feature
         {
             this.currentRenderTarget = currentTarget;
             my_level = new LEVEL[maxLevel];
-            for (int t = 0; t < maxLevel; t++)//申请32个ID的，up和down各16个，用这个id去代替临时RT来使用
+            for (int t = 0; t < maxLevel; t++)//32 ID: up & down = 16 each, use id instead of temp RT
             {
                 my_level[t] = new LEVEL
                 {
@@ -90,43 +90,43 @@ public class DualBlurRendererFeature : ScriptableRendererFeature
             if (!dualBlurVolume.IsActive()) { return; }
             
             // Grab a command buffer. We put the actual execution of the pass inside of a profiling scope.
-            CommandBuffer cmd = CommandBufferPool.Get(ProfilerRenderTag);//定义cmd
-            DualKawaseBlurMaterial.SetFloat("_BlurRadiusOffset",dualBlurVolume.BlurRadius.value);//指定材质参数
-            //cmd.SetGlobalFloat("_BlurRadiusOffset",dualBlurVolume.BlurRadius.value);//设置模糊,但是我不想全局设置怕影响其他的shader，所以注销它了用上面那个，但是cmd这个性能可能好些？
-            RenderTextureDescriptor opaquedesc = renderingData.cameraData.cameraTargetDescriptor;//定义屏幕图像参数结构体
-            int width = opaquedesc.width/(int)dualBlurVolume.downSample.value;//第一次降采样是使用的参数，后面就是除2去降采样了
+            CommandBuffer cmd = CommandBufferPool.Get(ProfilerRenderTag);
+            DualKawaseBlurMaterial.SetFloat("_BlurRadiusOffset",dualBlurVolume.BlurRadius.value);
+            //cmd.SetGlobalFloat("_BlurRadiusOffset",dualBlurVolume.BlurRadius.value);// better performance using global - will affect other shaders
+            RenderTextureDescriptor opaquedesc = renderingData.cameraData.cameraTargetDescriptor;
+            int width = opaquedesc.width/(int)dualBlurVolume.downSample.value;
             int height = opaquedesc.height/(int)dualBlurVolume.downSample.value;
             opaquedesc.depthBufferBits = 0;
 
             // Downsampling
-            RenderTargetIdentifier LastDown = currentRenderTarget;//把初始图像作为lastdown的起始图去计算
+            RenderTargetIdentifier LastDown = currentRenderTarget;// init RT
 
             for(int t = 0; t < dualBlurVolume.Iteration.value; t++)
             {
-                int midDown = my_level[t].down;//middle down ，即间接计算down的工具人ID
-                int midUp = my_level[t].up; //middle Up ，即间接计算的up工具人ID
-                cmd.GetTemporaryRT(midDown,width,height,0,FilterMode.Bilinear,RenderTextureFormat.ARGB32);//对指定高宽申请RT，每个循环的指定RT都会变小为原来一半
-                cmd.GetTemporaryRT(midUp,width,height,0,FilterMode.Bilinear,RenderTextureFormat.ARGB32);//同上，但是这里申请了并未计算，先把位置霸占了，这样在UP的循环里就不用申请RT了
-                cmd.Blit(LastDown,midDown,DualKawaseBlurMaterial,0);//计算down的pass
+                int midDown = my_level[t].down;//middle down
+                int midUp = my_level[t].up; //middle Up
+                cmd.GetTemporaryRT(midDown,width,height,0,FilterMode.Bilinear,RenderTextureFormat.ARGB32);// each iteration's RT will be half the original size
+                cmd.GetTemporaryRT(midUp,width,height,0,FilterMode.Bilinear,RenderTextureFormat.ARGB32);// in UP iteration: does not need to request RT 
+                cmd.Blit(LastDown,midDown,DualKawaseBlurMaterial,0);//down CALCULATION pass
                 LastDown = midDown;
-                width = Mathf.Max(width/2,1);//每次循环都降尺寸
+                width = Mathf.Max(width/2,1);// Reduce size for each iteration
                 height = Mathf.Max(height/2,1);
             }
 
             // UpSampling
-            int lastUp = my_level[dualBlurVolume.Iteration.value-1].down;//把down的最后一次图像当成up的第一张图去计算up
-            for(int j = dualBlurVolume.Iteration.value-2; j >= 0; j--)//这里减2是因为第一次已经有了要减去1，但是第一次是直接复制的，所以循环完后还得补一次up
+            int lastUp = my_level[dualBlurVolume.Iteration.value-1].down; //use down's last img as up's 1st img
+            for(int j = dualBlurVolume.Iteration.value-2; j >= 0; j--) // -2: 1st time -1, but 1st time is a copy, need to add up after iteration 
             {
                 int midUp = my_level[j].up;
-                cmd.Blit(lastUp,midUp,DualKawaseBlurMaterial,1);//这里直接开干就是因为在down过程中已经把RT的位置霸占好了，这里直接用，不虚
+                cmd.Blit(lastUp,midUp,DualKawaseBlurMaterial,1);
                 lastUp = midUp;
             }
             
-            cmd.Blit(lastUp,currentRenderTarget,DualKawaseBlurMaterial,1);//补一次up，顺便就输出了
+            cmd.Blit(lastUp,currentRenderTarget,DualKawaseBlurMaterial,1);
             
             // Execute the command buffer and release it
-            context.ExecuteCommandBuffer(cmd);//执行命令缓冲区的该命令
-            CommandBufferPool.Release(cmd);//释放cmd
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
         }
         
         
@@ -138,7 +138,7 @@ public class DualBlurRendererFeature : ScriptableRendererFeature
             if (cmd == null) throw new ArgumentNullException("cmd");
         
             // Since we created a temporary render texture in OnCameraSetup, we need to release the memory here to avoid a leak.
-            for(int k = 0;k<dualBlurVolume.Iteration.value;k++)//清RT，防止内存泄漏
+            for(int k = 0;k<dualBlurVolume.Iteration.value;k++)// clear RT
             {
                 cmd.ReleaseTemporaryRT(my_level[k].up);
                 cmd.ReleaseTemporaryRT(my_level[k].down);
